cc4abe1322f957636aeea6e9d1c0945b4d1db5d4
Pavel <kolesnik_pasha_9@mail.ru>
2022-01-30 21:04:44 +0300

    verify

--- final_work/Program.cs
+++ final_work/Program.cs
@@ -9,3 +9,7 @@ string[] array = { "eufh49", "2f4", "23r5", "p4", "403kg", "4))" };
 // Формируем новый массив согласно условию
 string[] new_array = Array.FindAll(array, a => a.Length <= 3);
 foreach (var a in new_array);
+
+// Выводим в консоль оба массива для проверки правильности решения
+Console.WriteLine(String.Join(" ",array));
+Console.WriteLine(String.Join(" ",new_array));
